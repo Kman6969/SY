a76c2b473cbab6e0b7efbdecc5c69473e145c7b4
Kman6969 <55087020+Kman6969@users.noreply.github.com>
2020-08-05 00:15:35 +0530

    Add files via upload

---
+++ Hrishikesh_Wavhal_Ass1/11/CtoF.java
@@ -0,0 +1,9 @@
+class CtoF{  
+   public static void main (String args[]){
+	   float Fahrenheit, Celsius;  
+         Celsius= 13.00f;  
+         Fahrenheit =((Celsius*9)/5)+32;  
+		System.out.println("Temperature in Celcius is: "+Celsius);
+		 System.out.println("Temperature in Fahrenheit is: "+Fahrenheit);  
+    }
+}  

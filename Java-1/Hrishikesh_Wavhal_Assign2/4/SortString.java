21dbb5dacdf81ff1ac0c2e4d82319895e8c8781a
neptune-sphere <55087020+neptune-sphere@users.noreply.github.com>
2020-08-25 13:14:07 +0530

    Update SortString.java

--- Java-1/Hrishikesh_Wavhal_Assign2/4/SortString.java
+++ Java-1/Hrishikesh_Wavhal_Assign2/4/SortString.java
@@ -10,12 +10,12 @@ class MaxCLA{
 		
 		String t;
 		
-		for(i=0;i<3-1;i++){
-			for(j=0;j<3-i-1;j++){
-				if(args[j].length > args[j+1].length){
-					t=args[j];
-					args[j]=args[j+1];
-					args[j+1]=t;
+		for(i=0;i<3;i++){
+			for(j=i+1;j<3;j++){
+				if(args[i].compareto(args[j])>0){
+					t=args[i];
+					args[i]=args[j];
+					args[j]=t;
 				}
 			}
 		}
